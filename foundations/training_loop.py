8f8e41889f6ea924f132a51ee79116c2c067fe84
simonshi-stu <80301915+simonshi-stu@users.noreply.github.com>
2026-09-25 14:13:27 +0800

    Implement Training loop (Training 1/4)

---
+++ foundations/training_loop.py
@@ -0,0 +1,31 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import Tuple
+
+
+class Solution:
+    def train(self, X: NDArray[np.float64], y: NDArray[np.float64], epochs: int, lr: float) -> Tuple[NDArray[np.float64], float]:
+        # X: (n_samples, n_features)
+        # y: (n_samples, targets)
+        # epochs: number of training iterations
+        # lr: learning rate
+        #
+        # Model: y_hat = X @ w + b
+        # Loss: MSE = (1/n) * sum((y_hat - y)^2)
+        # Initialize w = zeros, b = 0
+        # return (np.round(w, 5), round(b, 5))
+        n_samples, n_features = X.shape
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
+        n_targets = y.shape[1]
+        w = np.zeros((n_features, n_targets))
+        b = np.zeros((1, n_targets))
+        for i in range(epochs):
+            y_hat = X @ w + b
+            L = np.mean((y_hat - y)**2)
+            error = y_hat - y
+            gradient_w = (2 / n_samples) * (X.T @ error)
+            gradient_b = (2 / n_samples) * np.sum(error, axis = 0, keepdims=True)
+            w = w - lr * gradient_w
+            b = b - lr * gradient_b
+        return (np.round(w, 5).flatten(), np.round(b, 5).item())
