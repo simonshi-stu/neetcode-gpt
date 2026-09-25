683e7abb88a171ed59308f5f93688a5e642fc490
simonshi-stu <80301915+simonshi-stu@users.noreply.github.com>
2026-09-25 14:17:15 +0800

    Update Training loop (Training 1/4)

--- foundations/training_loop.py
+++ foundations/training_loop.py
@@ -14,18 +14,33 @@ class Solution:
         # Loss: MSE = (1/n) * sum((y_hat - y)^2)
         # Initialize w = zeros, b = 0
         # return (np.round(w, 5), round(b, 5))
+
         n_samples, n_features = X.shape
-        if y.ndim == 1:
-            y = y.reshape(-1, 1)
-        n_targets = y.shape[1]
-        w = np.zeros((n_features, n_targets))
-        b = np.zeros((1, n_targets))
-        for i in range(epochs):
+        w = np.zeros(n_features)
+        b = 0.0
+        
+        for _ in range(epochs):
             y_hat = X @ w + b
-            L = np.mean((y_hat - y)**2)
             error = y_hat - y
             gradient_w = (2 / n_samples) * (X.T @ error)
-            gradient_b = (2 / n_samples) * np.sum(error, axis = 0, keepdims=True)
-            w = w - lr * gradient_w
-            b = b - lr * gradient_b
-        return (np.round(w, 5).flatten(), np.round(b, 5).item())
+            gradient_b = (2 / n_samples) * np.sum(error)
+            w -= lr * gradient_w
+            b -= lr * gradient_b
+        return np.round(w, 5), np.round(b, 5)
+  
+        #换维度
+        # n_samples, n_features = X.shape
+        # if y.ndim == 1:
+        #     y = y.reshape(-1, 1)
+        # n_targets = y.shape[1]
+        # w = np.zeros((n_features, n_targets))
+        # b = np.zeros((1, n_targets))
+        # for i in range(epochs):
+        #     y_hat = X @ w + b
+        #     L = np.mean((y_hat - y)**2)
+        #     error = y_hat - y
+        #     gradient_w = (2 / n_samples) * (X.T @ error)
+        #     gradient_b = (2 / n_samples) * np.sum(error, axis = 0, keepdims=True)
+        #     w = w - lr * gradient_w
+        #     b = b - lr * gradient_b
+        # return (np.round(w, 5).flatten(), np.round(b, 5).item())
